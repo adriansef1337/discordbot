97ec79072eb6f4dc057718f6ecd384949b760378
adriansef1337 <92333381+adriansef1337@users.noreply.github.com>
2021-10-12 00:26:12 +0300

    Delete events directory

--- events/error.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2020-2021, Bastian Leicht <mail@bastianleicht.de>
- *
- * PDX-License-Identifier: BSD-2-Clause
- */
-// jshint esversion: 8
-module.exports = (client, error) => {
-    console.log(client.currentDate + ` | Error has been issued: ${error}`);
-};
